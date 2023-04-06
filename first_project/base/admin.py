9d32ab8c23eb7c2bd26caece12f535e944bc5405
dossmanos <126196979+dossmanos@users.noreply.github.com>
2023-04-06 18:09:52 +0200

    Delete first_project directory

--- first_project/base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import ChatRoom, ChatTopic, SystemMessage, User
-
-admin.site.register(User)
-admin.site.register(ChatRoom)
-admin.site.register(ChatTopic)
-admin.site.register(SystemMessage)
